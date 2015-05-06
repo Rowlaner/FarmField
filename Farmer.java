026310d73f507ba62c25269dbef20878d91088a5
Rishi Verma <r_verma2002@hotmail.com>
2015-05-06 15:54:09 +0100

    Update Farmer.java

--- Farmer.java
+++ Farmer.java
@@ -1,5 +1,12 @@
 public class Farmer 
 {
+    private Actor actor;
+  
+    public Farmer()
+    {
+        actor.actorType = ActorType.Farmer.ToString();
+    }
+    
     public void move()
     {
         Location newLocation = Field.freeAdjacentLocation();
