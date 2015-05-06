5db69fb948aaf2866bccd20384d944f41406915e
Rishi Verma <r_verma2002@hotmail.com>
2015-05-06 14:32:30 +0100

    Create Actor.java

---
+++ Actor.java
@@ -0,0 +1,12 @@
+public class Actor
+{
+  private int age;
+  private boolean alive = true;
+  
+  // (row, column)
+  private Location location;
+  
+  // must be < 1
+  // the range is a decimal from 0 to 1 in 0.1 increments
+  private int creationProbability;
+}
