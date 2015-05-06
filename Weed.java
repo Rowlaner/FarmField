5df2ab10d884869f49be03b156415e2dfcf88d8f
Rishi Verma <r_verma2002@hotmail.com>
2015-05-06 15:54:50 +0100

    Update Weed.java

--- Weed.java
+++ Weed.java
@@ -2,9 +2,11 @@ public class Weed extends Actor
 {
   // the age at which the Weed can begin spreading.
   private static final int weedSpreadingAge = 20;
- 
+  
+  private Actor actor;
+  
   public Weed()
   {
-    // set enum type as the ActorType value...
+    actor.actorType = ActorType.Weed.ToString();
   }
 }
